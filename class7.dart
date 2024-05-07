09ab4e7e1f9e2f715f3a2867e906abc5c57fdc9d
areebabahadur <areebabahadur2@gmail.com>
2024-05-07 21:25:27 +0500

    class7

---
+++ class7.dart
@@ -0,0 +1,19 @@
+class Humans{
+  String? name;
+  int? numberoflegs;
+  int?numberofhands;
+  
+  void display(){
+    print ("Humans name : $name.");
+    print ("number of legs : $numberoflegs.");
+    print ("number of hands : $numberofhands.");
+  }
+}
+
+void main (){
+  Humans human = Humans();
+  human.name ="abc";
+  human.numberoflegs = 2;
+  human.numberofhands = 2;
+  human.display();
+}
