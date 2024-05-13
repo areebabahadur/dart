1d2520ada113854524cb864695f7edb2da592148
areebabahadur <areebabahadur2@gmail.com>
2024-05-13 18:22:42 +0500

    some text

---
+++ inheritance.dart
@@ -0,0 +1,33 @@
+class Animal{
+  void walk (){
+    print("Animal walk");
+  }
+}
+class animals extends Animal{
+  void speak (){
+    print("animal is speak");
+  }
+}
+class animal extends Animal{
+  void ear (){
+    print ("animal has ear");
+  }
+}
+class aniimal extends Animal{
+  void eyes (){
+    print("animal has eyes");
+  }
+}
+class aanimal extends Animal{
+  void legs (){
+    print ("animal has legs");
+  }
+}
+
+void main (){
+  Animal p = Animal();
+  p.walk();
+
+  
+
+}
